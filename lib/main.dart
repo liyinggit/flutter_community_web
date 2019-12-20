0e26c293b98545a8ce12dfa2c691486f09b5dec9
liyinggit <2501830002@qq.com>
2019-12-20 18:02:38 +0800

    11

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,  //去除右上角的debug标签
-      home: One(),
-      routes: <String,WidgetBuilder>{
-        "login":(BuildContext context) => login(),  //登录页面
-        "home":(BuildContext context) => home(),   //主页
-        "cellList":(BuildContext context) =>cellList(),   //小区列表
-        "cellInformation":(BuildContext context) => cellInformation(), //小区信息编辑
-        "openRecord":(BuildContext context) => openRecord(),
+      debugShowCheckedModeBanner: false,
+      //去除右上角的debug标签
+      home: login(),
+      routes: <String, WidgetBuilder>{
+        "login": (BuildContext context) => login(),
+        //登录页面
+        "home": (BuildContext context) => home(),
+        //主页
+        "cellList": (BuildContext context) => cellList(),
+        //小区列表
+        "cellInformation": (BuildContext context) => cellInformation(),
+        //小区信息编辑
+        "openRecord": (BuildContext context) => openRecord(),
 
         //学习的部分
-        "content":(BuildContext context) =>content(),
-        "Two":(BuildContext context) => Two(),
+        "content": (BuildContext context) => content(),
+        "Two": (BuildContext context) => Two(),
       },
+//      onGenerateRoute: (RouteSettings settings) {
+//        return MaterialPageRoute(builder: (context) {
+//          String routeName = settings.name;
+//          print(routeName);
+//          // 如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，
+//          // 引导用户登录；其它情况则正常打开路由。
+//        });
+//      },
     );
   }
 }
-
