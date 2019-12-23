a875e45fb4bc676cbb18bfe19734ec3bf6c30741
liyinggit <2501830002@qq.com>
2019-12-23 11:42:26 +0800

    添加了table

--- lib/ui/openRecord.dart
+++ lib/ui/openRecord.dart
@@ -1,9 +1,72 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_community_web/common/adaptive.dart';
-import 'package:flutter_community_web/common/commonHeader.dart';
 
 import 'home.dart';
+
+class Dessert {
+  Dessert(this.openerId, this.deviceId, this.type, this.address, this.time,
+      this.status);
+
+  final String openerId;
+  final String deviceId;
+  final String type;
+  final String address;
+  final String time;
+  final String status;
+
+  bool selected = false;
+}
+
+class DessertDataSource extends DataTableSource {
+  final List<Dessert> _desserts = <Dessert>[
+    Dessert('1', 'D201990', '刷卡', '祈福新村摘星阁A栋', '2019-12-16 12:00', '待审批'),
+    Dessert('2', 'D201990', '人脸', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('3', 'D201990', '密码', '祈福新村摘星阁A栋', '2019-12-16 12:00', '拒绝'),
+    Dessert('4', 'D201990', '一键开门', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('5', 'D201990', '一键开门', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('1', 'D201990', '刷卡', '祈福新村摘星阁A栋', '2019-12-16 12:00', '待审批'),
+    Dessert('2', 'D201990', '人脸', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('3', 'D201990', '密码', '祈福新村摘星阁A栋', '2019-12-16 12:00', '拒绝'),
+    Dessert('4', 'D201990', '一键开门', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('5', 'D201990', '一键开门', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('1', 'D201990', '刷卡', '祈福新村摘星阁A栋', '2019-12-16 12:00', '待审批'),
+    Dessert('2', 'D201990', '人脸', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('3', 'D201990', '密码', '祈福新村摘星阁A栋', '2019-12-16 12:00', '拒绝'),
+    Dessert('4', 'D201990', '一键开门', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+    Dessert('5', 'D201990', '一键开门', '祈福新村摘星阁A栋', '2019-12-16 12:00', '同意'),
+  ];
+
+
+  int _selectedCount = 0;
+
+  @override
+  DataRow getRow(int index) {
+    assert(index >= 0);
+    if (index >= _desserts.length) return null;
+    final Dessert dessert = _desserts[index];
+    return DataRow.byIndex(
+      index: index,
+      cells: <DataCell>[
+        DataCell(Text('${dessert.openerId}')),
+        DataCell(Text('${dessert.deviceId}')),
+        DataCell(Text('${dessert.type}')),
+        DataCell(Text('${dessert.address}')),
+        DataCell(Text('${dessert.time}')),
+        DataCell(Text('${dessert.status}')),
+      ],
+    );
+  }
+
+  @override
+  int get rowCount => _desserts.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedCount;
+}
 
 class openRecord extends StatefulWidget {
   @override
@@ -42,6 +105,9 @@ class _openRecordState extends State<openRecord> {
     final bool isDesktop = isDisplayDesktop(context);
 
     final _media = MediaQuery.of(context).size;
+
+    int _rowsPerPage = 5;
+    final DessertDataSource _dessertsDataSource = DessertDataSource();
 
     ///区域
     Widget positionArea = Container(
@@ -164,7 +230,7 @@ class _openRecordState extends State<openRecord> {
 
     ///时间选择
     Widget timeText = Container(
-      width: 0.3 * _media.width,
+//      width: 0.8 * _media.width,
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
@@ -172,27 +238,83 @@ class _openRecordState extends State<openRecord> {
             "时间: ",
           ),
           Container(
-              margin: EdgeInsets.only(left: 10),
-              width: 0.2 * _media.width,
-              child: InkWell(
-                onTap: () {
-                  _selectDate(context);
-                },
-                child: DropdownButtonHideUnderline(
-                  child: InputDecorator(
-                    decoration: const InputDecoration(
-                        contentPadding: EdgeInsets.zero,
-                        border: OutlineInputBorder(),
-                        suffixIcon: Icon(Icons.arrow_drop_down_circle)),
-                  ),
+            margin: EdgeInsets.only(left: 10),
+            width: 0.2 * _media.width,
+            child: InkWell(
+              onTap: () {
+                _selectDate(context);
+              },
+              child: DropdownButtonHideUnderline(
+                child: InputDecorator(
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.zero,
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.arrow_drop_down_circle)),
                 ),
-              ))
+              ),
+            ),
+          ),
+          Text("~"),
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            width: 0.2 * _media.width,
+            child: InkWell(
+              onTap: () {
+                _selectDate(context);
+              },
+              child: DropdownButtonHideUnderline(
+                child: InputDecorator(
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.zero,
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.arrow_drop_down_circle)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    ///开门记录table
+    Widget openRecordTable = Container(
+      width: _media.width * 0.45,
+      height: _media.height * 0.5,
+      child: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: <Widget>[
+          PaginatedDataTable(
+            header: const Text('开门记录'),
+            rowsPerPage: _rowsPerPage,
+            columns: <DataColumn>[
+              DataColumn(
+                label: const Text('开门者id'),
+              ),
+              DataColumn(
+                label: const Text('设备号'),
+              ),
+              DataColumn(
+                label: const Text('开门方式'),
+              ),
+              DataColumn(
+                label: const Text('设备地址'),
+              ),
+              DataColumn(
+                label: const Text('来访时间'),
+              ),
+              DataColumn(
+                label: const Text('状态'),
+              ),
+            ],
+            source: _dessertsDataSource,
+          ),
         ],
       ),
     );
 
     ///主要的内容
     Widget body = Container(
+      alignment: Alignment.center,
       margin: EdgeInsets.only(top: 20),
       child: Column(
         children: <Widget>[
@@ -201,7 +323,14 @@ class _openRecordState extends State<openRecord> {
             height: 20,
           ),
           deviceText,
-          timeText
+          SizedBox(
+            height: 20,
+          ),
+          timeText,
+          SizedBox(
+            height: 20,
+          ),
+          openRecordTable
         ],
       ),
     );
@@ -236,7 +365,7 @@ class _openRecordState extends State<openRecord> {
             title: "开门记录",
           ),
           drawer: ListDrawer(),
-          body:body,
+          body: body,
         ),
       );
     }
