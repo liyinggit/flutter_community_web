674c3b0c6150838f4ac081ab055234f7947957b8
liyinggit <2501830002@qq.com>
2019-12-24 16:34:12 +0800

    调整了小区列表的布局

--- lib/ui/cellList.dart
+++ lib/ui/cellList.dart
@@ -17,10 +17,85 @@ class _bodyState extends State<body> {
     final _media = MediaQuery.of(context).size;
     print(_media);
 
+    ///电脑图标区域
+    Widget iconsAreaDeskTop = Container(
+      width: 0.1 * _media.width,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.border_color,
+              color: Colors.cyan,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, "cellInformation");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.business,
+              color: Colors.green,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, "addInfo");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.delete,
+              color: Colors.redAccent,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+
+    ///手机图标区域
+    Widget iconsAreaPhone = Container(
+      width: 0.1 * _media.width,
+      alignment: Alignment.center,
+      child: Column(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.border_color,
+              color: Colors.cyan,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, "cellInformation");
+            },
+            iconSize: 20,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.business,
+              color: Colors.green,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, "addInfo");
+            },
+            iconSize: 20,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.delete,
+              color: Colors.redAccent,
+            ),
+            onPressed: () {},
+            iconSize: 20,
+          ),
+        ],
+      ),
+    );
+
     ///每个元素
     Widget item = Container(
-      width: 0.6 * _media.width,
-      height: 0.2 * _media.width,
+      width: (isDesktop) ? 0.6 * _media.width : 0.9 * _media.width,
+      constraints: BoxConstraints(minHeight: 100),
       margin: EdgeInsets.all(20.0),
       child: DecoratedBox(
         decoration: BoxDecoration(
@@ -31,63 +106,32 @@ class _bodyState extends State<body> {
         child: Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-              width: 0.3 * _media.width,
+              width: (isDesktop) ? 0.2 * _media.width : 0.3 * _media.width,
+              alignment: Alignment.center,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.all(20),
+                    padding:
+                        (isDesktop) ? EdgeInsets.all(20) : EdgeInsets.all(10),
                     child: Text(
                       "标题",
                       style: TextStyle(fontSize: 24),
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+                    padding: (isDesktop)
+                        ? EdgeInsets.fromLTRB(20, 0, 0, 20)
+                        : EdgeInsets.fromLTRB(10, 0, 0, 10),
                     child: Text("这是一个副标题"),
                   ),
                 ],
               ),
             ),
-            Container(
-              width: 0.1 * _media.width,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.border_color,
-                      color: Colors.cyan,
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "cellInformation");
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.business,
-                      color: Colors.green,
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "addInfo");
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.redAccent,
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
+            (isDesktop) ? iconsAreaDeskTop : iconsAreaPhone,
             Container(
               width: 0.2 * _media.width,
               height: 0.2 * _media.width,
@@ -101,51 +145,47 @@ class _bodyState extends State<body> {
     );
 
     Widget body = Container(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            width: _media.width,
-            height: _media.height * 0.8,
-            child: ListView.separated(
-              itemBuilder: (BuildContext context, int index) {
-                return Center(
-                  child: item,
-                );
-              },
-              itemCount: 5,
-              separatorBuilder: (BuildContext context, int index) {
-                return Divider(
-                  color: Colors.white,
-                  height: 10,
-                );
-              },
-            ),
+        width: _media.width,
+        height: _media.height * 0.8,
+        child: Scaffold(
+          body: ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              return Center(
+                child: item,
+              );
+            },
+            itemCount: 10,
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider(
+                color: Colors.white,
+                height: 5,
+              );
+            },
           ),
-          Center(
-            child: Container(
-              width: 0.6 * _media.width,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 10, color: Colors.black12, spreadRadius: 2)
-                ],
-                color: Colors.grey[500],
-              ),
-              child: IconButton(
-                icon: Icon(Icons.add),
-                color: Colors.green,
-                iconSize: 100,
-                onPressed: () {
-                  Navigator.of(context).pushNamed("cellInformation");
-                },
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+          floatingActionButton: (isDesktop)
+              ? FloatingActionButton.extended(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "cellInformation");
+                  },
+                  label: Text(
+                    "添加",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                )
+              : FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "cellInformation");
+                  },
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.lightBlue,
+                  ),
+                ),
+        ));
     return body;
   }
 }
@@ -166,33 +206,32 @@ class cellList extends StatelessWidget {
             VerticalDivider(width: 1),
             Expanded(
               child: Scaffold(
-                  appBar: AdaptiveAppBar(
-                    isDesktop: isDesktop,
-                    title: "小区列表",
-                  ),
-                  body: Navigator(
-                    initialRoute: 'cellList',
-                    onGenerateRoute: (RouteSettings settings) {
-                      WidgetBuilder builder;
-                      switch (settings.name) {
-                        case 'cellList':
-                          builder = (BuildContext _) => body();
-                          break;
-                        case 'cellInformation':
-                          builder = (BuildContext _) => cellInformation();
-                          break;
-                        case "addInfo":
-                          builder = (BuildContext _) => addInfo();
-                          break;
-                        default:
-                          throw Exception('Invalid route: ${settings.name}');
-                      }
-                      return MaterialPageRoute(
-                          builder: builder, settings: settings);
-                    },
-                  ),
+                appBar: AdaptiveAppBar(
+                  isDesktop: isDesktop,
+                  title: "小区列表",
+                ),
+                body: Navigator(
+                  initialRoute: 'cellList',
+                  onGenerateRoute: (RouteSettings settings) {
+                    WidgetBuilder builder;
+                    switch (settings.name) {
+                      case 'cellList':
+                        builder = (BuildContext _) => body();
+                        break;
+                      case 'cellInformation':
+                        builder = (BuildContext _) => cellInformation();
+                        break;
+                      case "addInfo":
+                        builder = (BuildContext _) => addInfo();
+                        break;
+                      default:
+                        throw Exception('Invalid route: ${settings.name}');
+                    }
+                    return MaterialPageRoute(
+                        builder: builder, settings: settings);
+                  },
+                ),
               ),
-
             )
           ],
         ),
